7da289aad52a44c6be1c2566f2b1a0d617d24b18
Dev-PunChi <dev.punchi.84@gmail.com>
2021-01-30 01:44:01 +0900

    create-react-app & client / server folder change

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const app = express();
 const port = 3000;
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const { auth } = require("./middleware/auth");
-const { User } = require("./models/User");
-const config = require("./config/key");
+const { auth } = require("./server/middleware/auth");
+const { User } = require("./server/models/User");
+const config = require("./server/config/key");
 
 // application/x-www-from-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
