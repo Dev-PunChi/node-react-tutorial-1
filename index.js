fda32ba41b76aa867d97faee82d63e9c54057a39
Dev-PunChi <dev.punchi.84@gmail.com>
2021-01-29 14:51:17 +0900

    mongoose User Schema add

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const port = 3000;
@@ -9,3 +10,12 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+mongoose
+  .connect("mongodb://root:1q2w3e4r@112.153.1.31:27018", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "test",
+  })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((error) => console.log(error));
